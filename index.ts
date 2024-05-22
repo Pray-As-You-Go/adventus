13e732f400b7d12085a18f716b5b526c0652cc00
Matt McElwee <matt.mcelwee@jointhebibleproject.com>
2024-05-22 06:18:42 -0500

    feat: added exports

--- index.ts
+++ index.ts
@@ -1,18 +1,3 @@
-import { getBaseYearSet } from "./lib/getBaseYearSet";
-import { AdventusDate } from "./types";
-import { fillInDays } from "./lib/fillInDays";
-import { differenceInDays } from "date-fns";
-
-export const today = (date: Date): AdventusDate => {
-  const churchYear = fillInDays(getBaseYearSet(date.getFullYear()));
-  const firstDate = churchYear[0];
-
-  const dateIdx = differenceInDays(date, firstDate.date);
-  return churchYear[dateIdx];
-};
-
-(() => {
-  const [date] = process.argv.slice(2);
-  const day = today(new Date(date));
-  console.log(day);
-})();
+export { today } from "./today";
+export type { AdventusDate } from "./types";
+export { formatOrdinalNumber } from "./lib/ordinalNumber";
